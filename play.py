5a364b2b5d3511cedb37ac0bb2233b2254fc7259
Reed La Botz <reedlabotz@gmail.com>
2012-10-11 20:54:54 -0500

    1

--- play.py
+++ play.py
@@ -1,14 +1,23 @@
 import requests
 import json
 import time
-from analyzehand import whatwedo
+from analyzehand import whatwedo, odds
 
 PLAYER_KEY = '153af1f1d99f9f3f740d6f6a8ecdcd56'
 NAME = "RJ_Squared"
 GAME_ID = 660015106
 SLEEP_TIME = 1
-GET_URL = "http://no-limit-code-em.com/sandbox/current_turn" #"http://no-limit-code-em.com/sandbox/game_state"
-POST_URL = "http://no-limit-code-em.com/sandbox/player_action" #"http://no-limit-code-em.com/sandbox/player"
+GET_URL = "http://no-limit-code-em.com/sandbox/current_turn" #"http://no-limit-code-em.com/game_state"
+POST_URL = "http://no-limit-code-em.com/sandbox/player_action" #"http://no-limit-code-em.com/player"
+
+
+PLAYER_KEY = '6b55c1ecc710ba942137d41c3f1ac255'
+NAME = "script_it"
+GAME_ID = 660015106
+SLEEP_TIME = 1
+GET_URL = "http://treydev-poker.herokuapp.com/game_state"
+POST_URL = "http://treydev-poker.herokuapp.com/player_action" 
+
 
 class Game:
    def __init__(self):
@@ -33,51 +42,59 @@ class Game:
       payload = {'name': NAME, 'game_id': GAME_ID, 'player_key': PLAYER_KEY}
       r = requests.get(GET_URL, params=payload)
       data = json.loads(r.text)
-      self.current_player = data['current_player']
-      self.replacement = data['replacement']
-      self.hand = data['hand']
-      self.bet = data['bet']
-      self.stack = data['stack']
-      self.pot = data['pot']
-      self.min_bet = data['min_bet']
-      self.max_bet = data['max_bet']
-      self.max_raise = data['max_raise']
-      self.betting_summary = data['betting_summary']
-      self.replacement_summary = data['replacement_summary']
-      self.round_summary = data['round_summary']
-      self.play = data['play']
-      self.waiting = data['waiting']
+      print data
+      try:
+         self.replacement = data['replacement']
+         self.hand = data['hand']
+         self.bet = data['bet']
+         self.stack = data['stack']
+         self.pot = data['pot']
+         self.min_bet = data['min_bet']
+         self.max_bet = data['max_bet']
+         self.max_raise = data['max_raise']
+         self.round_summary = data['round_summary']
+         self.play = data['play']
+      except:
+         pass
 
-      self.replace()
-      if self.play:
-         self.play_move()
-      elif self.replacement:
-         self.replace()
-      else:
-         print "Current: %s"%self.current_player
-         print "stack: %d, pot: %d, bet: [%d-%d]"%(self.stack,self.pot,self.min_bet,self.max_bet)
-         print "summary: %s"%(",".join(self.round_summary))
-         print "-------------------------"
-      time.sleep(SLEEP_TIME)
+      try:
+         if self.play:
+            self.play_move()
+         elif self.replacement:
+            self.replace()
+         time.sleep(SLEEP_TIME)
+      except:
+         pass
       self.get_game_state()
       
 
 
    def make_move(self,action,params):
+      print "@@ %s:%s"%(action,params)
       payload = {"name":NAME,"game_id":GAME_ID,"player_key":PLAYER_KEY,"player_action":action,"parameters":params} 
       headers = {'content-type': 'application/json'}
       r = requests.post(POST_URL, data=json.dumps(payload), headers=headers)
-      print r.text
 
    def play_move(self):
       print "### PLAYING"
-      if self.bet < self.min_bet:
-         need_to_raise = self.min_bet - self.bet
-         print "### options: raise, call, fold"
-         self.make_move("raise",need_to_raise)
-      else:
-         print "### options: "
+      o = odds(self.hand)
+      print "** odds: %s"%str(o)
+
+      if o < 0.1:
          self.make_move("fold","")
+      elif self.bet < self.min_bet:
+         need_to_raise = self.min_bet - self.bet
+         if o >= 0.9:
+            self.make_move("raise",self.max_bet-self.bet )
+         elif o > 0.5:
+            self.make_move("call","")
+         elif o > 0.3 and self.min_bet/self.stack < 0.5:
+            self.make_move("call","")
+         else:
+            self.make_move("fold","")
+      else:
+         self.make_move("bet",self.min_bet)
+
 
    def replace(self):
       print "### REPLACING"
